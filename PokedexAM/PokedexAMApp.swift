5d3b3e4dd381024f97b2213924c3f79e49522656
tcmayanar <maynardtyler5@gmail.com>
2021-10-20 09:27:33 -0500

    Initial Commit

---
+++ PokedexAM/PokedexAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAMApp.swift
+//  PokedexAM
+//
+//  Created by AMStudent on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
